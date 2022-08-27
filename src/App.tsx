6d36938c9ed62fbd537c25d95bd866611df52c95
tarri9192 <tarri9192@gmail.com>
2022-08-27 22:26:13 +0100

    solauth

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { FakeWalletAdapter, PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+import React, { FC, ReactNode, useMemo } from 'react';
+
+require('./App.css');
+require('@solana/wallet-adapter-react-ui/styles.css');
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context>
+      <Content />
+    </Context>
   );
 }
 
 export default App;
+
+const Context: FC<{ children: ReactNode }> = ({ children }) => {
+  // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+  const network = WalletAdapterNetwork.Devnet;
+
+  // You can also provide a custom RPC endpoint.
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+      () => [
+
+          new PhantomWalletAdapter(),
+      ],
+      []
+  );
+return (
+  <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+          <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+  </ConnectionProvider>
+);
+};
+
+const Content: FC = () => {
+return (
+  <div className="App">
+      <WalletMultiButton />
+  </div>
+);
+};
